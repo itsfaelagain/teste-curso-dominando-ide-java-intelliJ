dedee99fe1e094504a01cde8c057fc4b049fd0b9
Fael <itsfaelagain@gmail.com>
2021-12-27 18:19:28 -0200

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,11 +6,44 @@ public class PrimeiroPrograma {
 
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
-        /*int a = 5;
-        int b = 3;
 
-        System.out.println("Hello World! " + (a+b));*/
+        Livro livro1 = new Livro("O problema dos corpos", 300);
+        System.out.println(livro1);
+
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer nPages;
+
+    public Livro(String nome, Integer nPages) {
+        this.nome = nome;
+        this.nPages = nPages;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getnPages() {
+        return nPages;
+    }
+
+    public void setnPages(Integer nPages) {
+        this.nPages = nPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", nPages=" + nPages +
+                '}';
     }
 }
